fb0f458e77dbd8bc6a0c31209559f17872ea9a02
fiserv-coder <85881630+fiserv-coder@users.noreply.github.com>
2021-06-14 08:47:53 -0400

    Create Coding Challenge Repo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fiserv React Coding Challenge</h1>
+      {/* Using component lifecycle methods or hooks */}
+      {/* API for posts https://jsonplaceholder.typicode.com/posts */}
+        {/* 1. Using the API above on init of the app make a request and load users by their userID */}
+        {/* 2. Make each use selectable with a button  */}
+        {/* 3. Be able to select any user and view only their posts */}
+
+
+
+
+
+        {/* API for comments https://jsonplaceholder.typicode.com/comments */}
+          {/* 1. Using the UserId add all the comments from this API above into state using the postId*/}
+    </div>
+  );
+}
+
+export default App;
